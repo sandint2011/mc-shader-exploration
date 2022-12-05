2a9cb284f5c0e75fe84fea2be0e7a176c6822984
sandint2011 <71451968+sandint2011@users.noreply.github.com>
2022-12-05 12:52:03 -0600

    Remove gameboy/diffuse shader since it's not as good

--- Gameboy With Diffuse Lighting/shaders/distort.glsl
+++
@@ -1,5 +0,0 @@
-vec2 DistortPosition(in vec2 position){
-    float CenterDistance = length(position);
-    float DistortionFactor = mix(1.0f, CenterDistance, 0.9f);
-    return position / DistortionFactor;
-}
